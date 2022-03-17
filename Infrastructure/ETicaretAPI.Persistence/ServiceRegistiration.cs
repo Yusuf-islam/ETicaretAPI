b5f7872f5f8425612de05a4cd02b1b2c044a5df9
Yusuf-islam <samwatcouh@gmail.com>
2022-03-17 21:35:53 +0300

    Projenin GitHuba Yüklenmesi

---
+++ Infrastructure/ETicaretAPI.Persistence/ServiceRegistiration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ETicaretAPI.Application.Abstraction;
+using ETicaretAPI.Persistence.Concretes;
+using ETicaretAPI.Persistence.Context;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Persistence.Repositories;
+
+namespace ETicaretAPI.Persistence
+{
+    public static class ServiceRegistiration
+    {
+
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            ConfigurationManager configurationManager = new ConfigurationManager();
+            configurationManager.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../../Presentation/ETicaretAPI.API"));
+            configurationManager.AddJsonFile("appsettings.json");
+
+            services.AddDbContext<ETicaretAPIDbContext>(option => option.UseNpgsql(configurationManager.GetConnectionString("PostgreSQL")),ServiceLifetime.Singleton);
+            services.AddSingleton<ICustomerReadRepository, CustomerReadRepository>();
+            services.AddSingleton<ICustomerWriteRepository, CustomerWriteRepository>();
+            services.AddSingleton<IOrderReadRepository, OrderReadRepository>();
+            services.AddSingleton<IOrderWriteRepository, OrderWriteRepository>();
+            services.AddSingleton<IProductReadRepository, ProductReadRepository>();
+            services.AddSingleton<IProductWriteRepository, ProductWriteRepository>();
+
+        }
+    }
+}
---
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using ETicaretAPI.Application.Abstraction;
+using ETicaretAPI.Application.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        readonly private IProductWriteRepository _productWriteRepository;
+        readonly private IProductReadRepository _productReadRepository;
+        public ProductsController(IProductWriteRepository productWriteRepository,IProductReadRepository productReadRepository)
+        {
+            _productWriteRepository = productWriteRepository;   
+            _productReadRepository = productReadRepository; 
+
+        }
+
+        [HttpGet]
+        public async void Get()
+        {
+
+            await _productWriteRepository.AddRangeAsync(new()
+            {
+                new() {  Name = "Product 1", Price = 100, CreatedDate = DateTime.UtcNow, Stock = 10 },
+                new() {  Name = "Product 2", Price = 200, CreatedDate = DateTime.UtcNow, Stock = 20 },
+                new() {  Name = "Product 3", Price = 300, CreatedDate = DateTime.UtcNow, Stock = 30 },
+                new() {  Name = "Product 4", Price = 400, CreatedDate = DateTime.UtcNow, Stock = 40 }
+
+            });
+            await  _productWriteRepository.SaveAsync();
+        }
+
+
+    }
+}
